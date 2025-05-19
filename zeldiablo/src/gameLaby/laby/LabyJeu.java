6d8dd6d158e2437514e950e2eb16991d8d84dd36
PlumeDeRenard54 <tom.aubert54@outlook.fr>
2025-05-19 10:28:35 +0200

    AjoutLabyJeu

---
+++ zeldiablo/src/gameLaby/laby/LabyJeu.java
@@ -0,0 +1,21 @@
+package gameLaby.laby;
+
+import moteurJeu.Clavier;
+import moteurJeu.Jeu;
+
+public class LabyJeu implements Jeu {
+    @Override
+    public void update(double secondes, Clavier clavier) {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public boolean etreFini() {
+        return false;
+    }
+}
