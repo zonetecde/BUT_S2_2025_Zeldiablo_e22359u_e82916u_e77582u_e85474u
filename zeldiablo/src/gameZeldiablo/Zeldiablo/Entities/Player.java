47fae5c4d91615de43a4ef10219a8ad0e4c2b8f2
PlumeDeRenard54 <tom.aubert54@outlook.fr>
2025-06-02 10:18:02 +0200

    Ajout de l'interface item

--- zeldiablo/src/gameZeldiablo/Zeldiablo/Entities/Player.java
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/Entities/Player.java
@@ -2,7 +2,11 @@ package gameZeldiablo.Zeldiablo.Entities;
 
 import gameZeldiablo.Zeldiablo.VariablesGlobales;
 
+import java.util.ArrayList;
+
 public class Player extends Entite {
+    private ArrayList
+
     public Player(int dx, int dy, int hp, int maxHp) {
         super(dx, dy, hp, maxHp);
     }
---
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/Items/item.java
@@ -0,0 +1,4 @@
+package gameZeldiablo.Zeldiablo.Items;
+
+public interface item {
+}
