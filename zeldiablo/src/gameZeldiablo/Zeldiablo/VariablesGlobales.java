be739aaf583d537e5e30fba4ac7c2c2a4a82d6e8
PlumeDeRenard54 <tom.aubert54@outlook.fr>
2025-06-02 11:46:26 +0200

    Fix variables globales

--- zeldiablo/src/gameZeldiablo/Zeldiablo/VariablesGlobales.java
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/VariablesGlobales.java
@@ -3,14 +3,23 @@ package gameZeldiablo.Zeldiablo;
 import javafx.scene.paint.Color;
 
 public class VariablesGlobales {
-    /**
-     * Constantes du jeu Zeldiablo
-     */
-    public static final Color COULEUR_VIDE = Color.WHITE;
-    public static final Color COULEUR_MUR = Color.BLACK;
-    public static final Color COULEUR_PIEGE = Color.PURPLE;
-    public static final int TAILLE_CASE = 40;
-    public static final double PV_BASE = 10;
-    public static final double DEGAT_BASE = 3;
+        /**
+         * Constantes du jeu Zeldiablo
+         */
+        //Couleurs
+        public static final Color COULEUR_VIDE = Color.WHITE;
+        public static final Color COULEUR_MUR = Color.BLACK;
+        public static final Color COULEUR_PIEGE = Color.PURPLE;
+
+        //Constantes
+        public static final int TAILLE_CASE = 40;
+        public static final double PV_BASE = 10;
+        public static final double DEGAT_BASE = 3;
     public static final int PROBA_MONSTRE = 40; // 1/40 de chance de générer un monstre
+
+        //VariablesMenu
+        public static final int COL_NUM_MENU = 6;
+        public static boolean MenuOuvert=false;
+        public static int curseur=0;
+
 }
