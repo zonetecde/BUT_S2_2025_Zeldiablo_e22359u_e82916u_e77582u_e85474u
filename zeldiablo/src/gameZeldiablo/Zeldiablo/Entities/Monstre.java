9936e2a4dc3ffea14344cd936f5dea706f54055e
zonetecde <zonetecde@gmail.com>
2025-06-03 08:30:13 +0200

    Correction de la hiérarchie des classes et de certains constructeur.

--- zeldiablo/src/gameZeldiablo/Zeldiablo/Entities/Monstre.java
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/Entities/Monstre.java
@@ -1,12 +1,18 @@
 package gameZeldiablo.Zeldiablo.Entities;
 
 import gameZeldiablo.Zeldiablo.StrategieDeplacement.DeplacementStrategie;
+import gameZeldiablo.Zeldiablo.VariablesGlobales;
 
 public class Monstre extends Entite {
     private DeplacementStrategie deplacementStrategie;
 
     public Monstre(int x, int y, double pv, double degat, DeplacementStrategie deplacementStrategie) {
         super(x, y, pv, degat);
+        this.deplacementStrategie = deplacementStrategie;
+    }
+
+    public Monstre(int x, int y, DeplacementStrategie deplacementStrategie) {
+        super(x, y, 3, VariablesGlobales.DEGAT_BASE);
         this.deplacementStrategie = deplacementStrategie;
     }
 }
--- zeldiablo/src/gameZeldiablo/Zeldiablo/Entities/MonstreStatique.java
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/Entities/MonstreStatique.java
@@ -1,9 +1,14 @@
 package gameZeldiablo.Zeldiablo.Entities;
 
 import gameZeldiablo.Zeldiablo.StrategieDeplacement.DeplacementStatique;
+import gameZeldiablo.Zeldiablo.VariablesGlobales;
 
 public class MonstreStatique extends Monstre {
     public MonstreStatique(int dx, int dy, double hp, double degat) {
         super(dx, dy, hp, degat, new DeplacementStatique());
+    }
+
+    public MonstreStatique(int dx, int dy){
+        super(dx, dy, new DeplacementStatique());
     }
 }
