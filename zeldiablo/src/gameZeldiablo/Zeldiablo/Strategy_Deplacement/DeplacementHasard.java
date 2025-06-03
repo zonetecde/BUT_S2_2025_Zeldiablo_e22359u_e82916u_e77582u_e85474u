6cff1533009ea444787cdd2475c6656027ec1ecb
Elias AMACH <elias.amach1@etu.univ-lorraine.fr>
2025-06-03 08:25:39 +0200

    Ajout de la classe DeplacementHasard et mise à jour de l'interface DeplacementStrategy pour inclure le paramètre Labyrinthe

---
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/Strategy_Deplacement/DeplacementHasard.java
@@ -0,0 +1,12 @@
+package gameZeldiablo.Zeldiablo.Strategy_Deplacement;
+
+import gameZeldiablo.Zeldiablo.Labyrinthe;
+
+public class DeplacementHasard implements DeplacementStrategy {
+
+    public void deplacement(Labyrinthe laby) {
+
+
+    }
+}
+
--- zeldiablo/src/gameZeldiablo/Zeldiablo/Strategy_Deplacement/DeplacementStrategy.java
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/Strategy_Deplacement/DeplacementStrategy.java
@@ -1,11 +1,13 @@
 package gameZeldiablo.Zeldiablo.Strategy_Deplacement;
 
+import gameZeldiablo.Zeldiablo.Labyrinthe;
+
 public interface DeplacementStrategy {
     /**
      * Déplace l'entité selon la stratégie de déplacement définie.
      **/
 
-    public void deplacement();
+    public void deplacement(Labyrinthe laby);
 
      }
 
