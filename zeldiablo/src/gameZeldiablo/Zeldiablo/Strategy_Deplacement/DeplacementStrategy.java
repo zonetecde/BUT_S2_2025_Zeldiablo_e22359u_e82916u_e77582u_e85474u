1b2a74187a79af7c8a45e687d77821f7a8158e45
Elias AMACH <elias.amach1@etu.univ-lorraine.fr>
2025-06-03 08:19:19 +0200

    Ajout de l'interface DeplacementStrategy pour définir les stratégies de déplacement

---
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/Strategy_Deplacement/DeplacementStrategy.java
@@ -0,0 +1,12 @@
+package gameZeldiablo.Zeldiablo.Strategy_Deplacement;
+
+public interface DeplacementStrategy {
+    /**
+     * Déplace l'entité selon la stratégie de déplacement définie.
+     **/
+
+    public void deplacement();
+
+     }
+
+
