bef109b21629157fb029e5df60ef55863f0d8a62
Elias AMACH <elias.amach1@etu.univ-lorraine.fr>
2025-06-03 08:47:56 +0200

    Code deplacementHasard

--- zeldiablo/src/gameZeldiablo/Zeldiablo/StrategieDeplacement/DeplacementHasard.java
+++ zeldiablo/src/gameZeldiablo/Zeldiablo/StrategieDeplacement/DeplacementHasard.java
@@ -1,12 +1,22 @@
-package gameZeldiablo.Zeldiablo.Strategy_Deplacement;
+package gameZeldiablo.Zeldiablo.StrategieDeplacement;
 
+import gameZeldiablo.Zeldiablo.Entities.Monstre;
 import gameZeldiablo.Zeldiablo.Labyrinthe;
 import gameZeldiablo.Zeldiablo.StrategieDeplacement.DeplacementStrategie;
 
 public class DeplacementHasard implements DeplacementStrategie {
 
-    public void deplacement(Labyrinthe laby) {
 
+    public void deplacement(Labyrinthe laby, Monstre monstre) {
+        int dx = (int) (Math.random() * 3) - 1; // -1, 0, ou 1
+        int dy = (int) (Math.random() * 3) - 1; // -1, 0, ou 1
+
+        if (dx == 0 && dy == 0) {
+            dx = 1;
+        }
+
+        monstre.setX(dx);
+        monstre.setY(dy);
 
     }
 }
